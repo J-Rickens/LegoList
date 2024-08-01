b7fed9f31e98792e52e29272eee2b895dc2373b1
J-Rickens <jrickens2019@gmail.com>
2024-08-01 12:28:44 -0400

    update login form syntax

--- login/index.php
+++ login/index.php
@@ -3,6 +3,8 @@
 	$urlLvl = 1;
 	$urlTitle = "login/Register";
 	include('../.templates/opener.tp.php');
+
+	// import regex patterns
 	include($urlReturn . ".regex/login.regex.php");
 
  ?>
@@ -18,8 +20,11 @@
 			<h4>Login</h4>
 			<p>Existing Builders Login Here!</p>
 			<form action="../.includes/login.inc.php" method="post">
-				<input type="text" name="usna" placeholder="Username" required>
-				<input type="password" name="pwd" placeholder="Password" required>
+				<?php // inputs for required feilds: username, password ?>
+				<input type="text" name="usna" placeholder="Username"
+					required>
+				<input type="password" name="pwd" placeholder="Password"
+					required>
 				<br>
 				<button type="submit" name="submit">LOGIN</button>
 			</form>
@@ -29,11 +34,20 @@
 			<h4>Register</h4>
 			<p>Don't have an account yet? Sign up here!</p>
 			<form action="../.includes/register.inc.php" method="post">
-				<input type="text" name="name" placeholder="Name" pattern="<?php echo LoginRegex::NAME; ?>" required title="<?php echo LoginRegex::NAMEDESCR; ?>">
-				<input type="email" name="email" placeholder="Email Address" required>
-				<input type="text" name="usna" placeholder="Username" pattern="<?php echo LoginRegex::USNA; ?>" required title="<?php echo LoginRegex::USNADESCR; ?>">
-				<input type="password" name="pwd" placeholder="Password" pattern="<?php echo LoginRegex::PWD; ?>" required title="<?php echo LoginRegex::PWDDESCR; ?>">
-				<input type="password" name="pwd2" placeholder="Repeat Password" required>
+				<?php // inputs for required feilds: name, email, username, password ?>
+				<input type="text" name="name" placeholder="Name"
+					pattern="<?php echo LoginRegex::NAME; ?>" title="<?php echo LoginRegex::NAMEDESCR; ?>"
+					required>
+				<input type="email" name="email" placeholder="Email Address"
+					required>
+				<input type="text" name="usna" placeholder="Username"
+					pattern="<?php echo LoginRegex::USNA; ?>" title="<?php echo LoginRegex::USNADESCR; ?>"
+					required>
+				<input type="password" name="pwd" placeholder="Password"
+					pattern="<?php echo LoginRegex::PWD; ?>" title="<?php echo LoginRegex::PWDDESCR; ?>"
+					required>
+				<input type="password" name="pwd2" placeholder="Repeat Password"
+					required>
 				<br>
 				<button type="submit" name="submit">SIGN UP</button>
 			</form>
