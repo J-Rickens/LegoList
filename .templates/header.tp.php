a2536d17ac8926f4c3ea36b2fc1ebe4567079541
J-Rickens <jrickens2019@gmail.com>
2024-08-05 14:26:08 -0400

    add addLego, addList to nav bar when logedin

--- .templates/header.tp.php
+++ .templates/header.tp.php
@@ -16,6 +16,8 @@
 				<ul>
 					<?php if (isset($_SESSION['uid'])) { ?>
 						<li><a href=<?php echo $urlReturn . "dashboard"; ?>><?php echo $_SESSION['name']; ?></a></li>
+						<li><a href=<?php echo $urlReturn . "add/lego"; ?>>Add Lego</a></li>
+						<li><a href=<?php echo $urlReturn . "add/list"; ?>>Add List</a></li>
 						<li><a href=<?php echo $urlReturn . ".includes/logout.inc.php"; ?>>LOGOUT</a></li>
 					<?php } else { ?>
 						<li><a href=<?php echo $urlReturn . "login"; ?>>login/register</a></li>
